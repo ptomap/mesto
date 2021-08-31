43ddbdc3971d52a937b1d3d73eff8b07e1f98071
Tamara <t.prisheltseva@yandex.ru>
2021-08-31 19:33:59 +0300

    Третья версия на отправку

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,19 +9,22 @@ let jobInput = document.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let jobName = document.querySelector('.profile__job');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent; // Значения полей jobInput и nameInput из верстки при октрытии попапа
   jobInput.value = jobName.textContent;
 }
-
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы на сервер
-  profileName.textContent = nameInput.value; // Новые значения из формы вставляем в вертку
-  jobName.textContent = jobInput.value;
-  togglePopup() // Вызываем функцию и убираем модификтаор, чтобы попап скрылся
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // отменяем стандартную отправку формы на сервер
+  profileName.textContent = nameInput.value; // Новые значения из формы вставляем в вертку
+  jobName.textContent = jobInput.value;
+  closePopup() // Вызываем функцию и убираем модификтаор, чтобы попап скрылся
+}
+
+openPopupButton.addEventListener('click', openPopup); //открываем попам кликом
+closePopupButton.addEventListener('click', closePopup); //закрываем попам кликом
 formElement.addEventListener('submit', formSubmitHandler);
