5fcf96baf6f9370a8d3b9a087d8f30128155ad1e
Tamara <t.prisheltseva@yandex.ru>
2021-12-09 00:33:54 +0300

    Первая версия

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -4,9 +4,11 @@ export default class Section {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
+  //отрисовка всех элементов, отрисовка каждого отдельного элемента
   renderItems() {
     this._renderedItems.forEach(item => this._renderer(item));
   }
+  //оторый принимает DOM-элемент и добавляет его в контейнер.
   addItem(element) {
     this._container.prepend(element);
   }
